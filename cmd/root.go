1ddcf32830db1ffea6e34175b86faf36b74511e9
yu.liu <liuyucupid@qq.com>
2022-09-01 22:45:23 +0800

    feat: 添加自定义关键字参数

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	gt "mimi/pkg"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -19,11 +20,16 @@ var rootCmd = &cobra.Command{
 	Long:  `代理服务器.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		port, _ := cmd.Flags().GetString("port")
+		key, _ := cmd.Flags().GetString("key")
 		ipt := &gt.Intercept{
 			Ip: "0.0.0.0:" + port,
 			HttpPackageFunc: func(pack *gt.HttpPackage) {
-				log.Println("json = ", pack.Json())
-				log.Printf("html = %s. gzip = %v", pack.Html(), pack.Gzip)
+				if strings.Index(pack.ContentType, "json") != -1 && strings.Contains(pack.Json(), key) {
+					log.Printf("json: url = %s\r\n ", pack.Url.String())
+				}
+				if strings.Index(pack.ContentType, "html") != -1 && strings.Contains(pack.Html(), key) {
+					log.Printf("html: url = %s\r\n ", pack.Url.String())
+				}
 			},
 		}
 		// 启动服务
@@ -45,6 +51,7 @@ func init() {
 	rootCmd.Version = version
 	rootCmd.Flags().BoolP("version", "v", false, "")
 	rootCmd.Flags().StringP("port", "p", "8111", "")
+	rootCmd.Flags().StringP("key", "k", "idCard", "")
 	rootCmd.AddCommand(versionCmd)
 
 }
